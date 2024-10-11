903c175ab0cbdaa1208140b2295e77f7911438e8
Simon Shen <shentiexiong@gmail.com>
2024-10-11 23:33:49 +1300

    Refactor BookDetails and products.ts files

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,35 +1,35 @@
-export const products = [
-  {
-    id: 1,
-    name: 'Continental ExtremeContact DWS06',
-    price: 799,
-    quantity: 2,
-    status: 'in Stock',
-    description:
-      'High-performance tire for sports cars, offering excellent grip and handling.',
-    imgaddress:
-      'https://dxm.contentcenter.michelin.com/api/wedia/dam/transform/b98rpyxf61b4xe194hr9qcgqyy/4w-238_3528700093414_tire_michelin_pilot-sport-4_245-slash-40-zr18-97y-xl_a_main_1-30_nopad.webp?t=resize&height=500',
-  },
-  {
-    id: 2,
-    name: 'Pirelli P Zero Nero',
-    price: 699,
-    quantity: 4,
-    status: '2 left',
-    description:
-      'remium tire designed for luxury vehicles, ensuring a smooth and quiet ride.',
-    imgaddress:
-      'https://www.wheels-alive.co.uk/wp-content/uploads/2020/05/Bridgestone-DriveGuard-full-tyre-scaled.jpg',
-  },
-  {
-    id: 3,
-    name: 'Hankook Ventus V12 evo2',
-    price: 299,
-    quantity: 0,
-    status: '4 left',
-    description:
-      'Ultra high-performance tire for superior handling and stability at high speeds.',
-    imgaddress:
-      'https://www.carpockets.co.ke/cdn/shop/articles/the-secret-numbers-on-your-tyres-and-what-they-mean-carpockets_817d93d5-bec3-42a4-ad28-4ed97ea8ea58.jpg?v=1707828269',
-  },
-];
+// export const products = [
+//   {
+//     id: 1,
+//     name: 'Continental ExtremeContact DWS06',
+//     price: 799,
+//     quantity: 2,
+//     status: 'in Stock',
+//     description:
+//       'High-performance tire for sports cars, offering excellent grip and handling.',
+//     imgaddress:
+//       'https://dxm.contentcenter.michelin.com/api/wedia/dam/transform/b98rpyxf61b4xe194hr9qcgqyy/4w-238_3528700093414_tire_michelin_pilot-sport-4_245-slash-40-zr18-97y-xl_a_main_1-30_nopad.webp?t=resize&height=500',
+//   },
+//   {
+//     id: 2,
+//     name: 'Pirelli P Zero Nero',
+//     price: 699,
+//     quantity: 4,
+//     status: '2 left',
+//     description:
+//       'remium tire designed for luxury vehicles, ensuring a smooth and quiet ride.',
+//     imgaddress:
+//       'https://www.wheels-alive.co.uk/wp-content/uploads/2020/05/Bridgestone-DriveGuard-full-tyre-scaled.jpg',
+//   },
+//   {
+//     id: 3,
+//     name: 'Hankook Ventus V12 evo2',
+//     price: 299,
+//     quantity: 0,
+//     status: '4 left',
+//     description:
+//       'Ultra high-performance tire for superior handling and stability at high speeds.',
+//     imgaddress:
+//       'https://www.carpockets.co.ke/cdn/shop/articles/the-secret-numbers-on-your-tyres-and-what-they-mean-carpockets_817d93d5-bec3-42a4-ad28-4ed97ea8ea58.jpg?v=1707828269',
+//   },
+// ];
