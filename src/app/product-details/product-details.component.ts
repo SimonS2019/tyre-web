5755d8e529b5fc73014d5a8266ec9af99b6d7049
Simon Shen <shentiexiong@gmail.com>
2024-10-12 16:21:04 +1300

    Refactor navbar in app.component.html and add car information for logged in users

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -32,7 +32,8 @@ export class ProductDetailsComponent implements OnInit {
 
   clickMe(product: Product) {
     window.alert(
-      product.name + 'Course videos are available in your location to purchase'
+      product.name +
+        ' fit your car and are available in your location to purchase'
     );
   }
 
