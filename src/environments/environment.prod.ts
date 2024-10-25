d26d3b223c4f92f2ff14305eb5cecf096f22c4d1
SimonS2019 <shentiexiong@gmail.com>
2024-10-25 17:10:07 +1300

    Connect to live server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  configUrl: 'https://your-production-url.com/'
+  // configUrl: 'https://your-production-url.com/'
+  configUrl: 'https://tyre-api-simons2019s-projects.vercel.app/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  configUrl: 'http://localhost:3000/'
+  // configUrl: 'http://localhost:3000/'
+  configUrl: 'https://tyre-api-simons2019s-projects.vercel.app/',
 };
 
 /*
