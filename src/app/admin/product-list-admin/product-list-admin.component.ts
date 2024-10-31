1b3f05441ff3ea1090c882318c9caf38b29857a2
SimonS2019 <shentiexiong@gmail.com>
2024-10-31 17:01:52 +1300

    Implement product list display with update and delete functionality in admin panel

--- src/app/admin/product-list-admin/product-list-admin.component.ts
+++ src/app/admin/product-list-admin/product-list-admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from '../admin.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-list-admin',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list-admin.component.css']
 })
 export class ProductListAdminComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private adminService: AdminService, private router: Router) { }
 
   ngOnInit(): void {
+    this.adminService.getAllProducts().subscribe(
+      (data: Product[]) => {
+        this.products = data;
+      },
+      (error) => {
+        console.error('Failed to load products', error);
+      }
+    );
   }
 
+  updateProduct(productId: string): void {
+    // this.router.navigate(['/product-update', productId]);
+    console.log('Update product with id: ', productId);
+    
+  }
+
+  deleteProduct(productId: string): void {
+    console.log('Delete product with id: ', productId);
+    
+    // this.adminService.deleteProduct(productId).subscribe(
+    //   () => {
+    //     this.products = this.products.filter(product => product.id !== productId);
+    //   },
+    //   (error) => {
+    //     console.error('Failed to delete product', error);
+    //   }
+    // );
+  }
 }
